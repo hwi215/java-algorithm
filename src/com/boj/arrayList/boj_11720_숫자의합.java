1991142e4f9a8cbf64093e1f1ce469881b9c099c
hwi215 <buc06137@naver.com>
2023-07-10 13:30:34 +0900

    [add] 배열, 형변환 관련 문제

---
+++ src/com/boj/arrayList/boj_11720_숫자의합.java
@@ -0,0 +1,26 @@
+package com.boj.arrayList;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class boj_11720_숫자의합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split("");
+
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += Integer.parseInt(s[i]);
+        }
+
+        System.out.println(sum);
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+}
