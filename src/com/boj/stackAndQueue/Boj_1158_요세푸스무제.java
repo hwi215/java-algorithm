7f00f6ac366771fbdbdf208ed97adaf102aff6e1
hwi215 <buc06137@naver.com>
2023-07-12 11:00:48 +0900

    [add] 큐 문제풀이

---
+++ src/com/boj/stackAndQueue/Boj_1158_요세푸스무제.java
@@ -0,0 +1,53 @@
+package com.boj.stackAndQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 원형큐
+ */
+public class Boj_1158_요세푸스무제 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        Queue<Integer> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<");
+
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+        for(int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        int count = 1;
+        while(!q.isEmpty()){
+            if(count == k){
+                if(q.size() == 1){
+                    sb.append(q.poll());
+
+                }else {
+                    sb.append(q.poll()).append(", ");
+                }
+                count = 1;
+
+            }else{
+                int target = q.poll();
+                q.offer(target);
+                count += 1;
+            }
+
+        }
+
+        sb.append(">");
+
+        System.out.println(sb);
+
+
+    }
+}
