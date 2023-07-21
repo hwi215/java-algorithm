796d6b5028aeb5ceb6fe027fcdccc7acf95e55bd
hwi215 <buc06137@naver.com>
2023-07-21 10:37:04 +0900

    [add] 유니온파인드 개념

---
+++ src/com/graph/UnionFind.java
@@ -0,0 +1,30 @@
+package com.graph;
+
+/**
+ * union연산: 노드 합치기
+ * find연산_ 루트 노드를 찾는 연산
+ *
+ * 크루스칼 알고리즘에서 사용
+ * 시간복잡도 O(1)
+ *
+ * 초기화 - 자기 자신을 연결함
+ * 부모와 자식 연결하기 - 유니온으로 연결
+ *
+ * 일반적인 함수보다 빠름!!!
+ *
+ *
+ * 1. Uniont
+ * 루트노드를 연결하여 결과
+여*/
+public class UnionFind {
+
+    /*
+
+    public static void main(String[] args) {
+        // 부모의 초기화
+        int max = int[N];
+        for(int i =0luM N-1)
+    }
+
+     */
+}
